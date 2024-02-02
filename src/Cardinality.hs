267504d667d7cb6f2580b0c9e0db41b2891da7cf
emmanuel <emmanuel@whetztone.net>
2024-02-02 12:43:35 +0000

    Added higher kinded type class for enumerable types.

--- src/Cardinality.hs
+++ src/Cardinality.hs
@@ -1,9 +1,29 @@
--- | The cardinality of a type is the number of values contained in the type.
-module Cardinality where
+{-| The cardinality of a type is the number of values contained in the type.
+-}
+module Cardinality (
+    -- * Version 0
+    -- $version0
+
+    -- * Version 1
+    -- $version1
+    GMyEnum(..)
+    ) where
 
 import Data.Proxy (Proxy)
 
+{- $version0 
+@
 class MyEnum a where
-    cardinality :: Proxy a -> Integer
-    toMyEnum    :: Integer -> a
-    fromMyEnum  :: a -> Integer
+    'cardinality' :: 'Proxy' a -> 'Integer'
+    'toMyEnum'    :: 'Integer' -> a
+    'fromMyEnum'  :: a -> 'Integer'
+@
+-}
+
+
+{- $version1
+-}
+class GMyEnum f where
+    gcardinality :: Proxy f -> Integer
+    toGMyEnum :: Integer -> f a
+    fromGMyEnum :: f a -> Integer
