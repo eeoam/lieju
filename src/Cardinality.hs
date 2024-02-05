b78b34baa4a94bcad23f8ae1dfba16aa455c3991
emmanuel <emmanuel@whetztone.net>
2024-02-05 11:39:58 +0000

    Implemented Demo module.

--- src/Cardinality.hs
+++ src/Cardinality.hs
@@ -3,10 +3,11 @@
 module Cardinality (
     -- * Version 0
     -- $version0
+      MyEnum(..)
+    , GMyEnum(..)
 
     -- * Version 1
     -- $version1
-    GMyEnum(..)
     ) where
 
 import Data.Proxy 
@@ -14,9 +15,21 @@ import Data.Proxy
     )
 
 import GHC.Generics
-    ( (:+:) (..)
+    ( Generic
+    , V1
+    , U1(..)
+    , K1(..)
+    , M1(..)
+    , (:+:) (..)
     , (:*:) (..)
+    , Rep
+    , from
+    , to
     )
+
+import Data.Kind ( Type )
+
+import Data.Void ( Void )
 
 {- $version0 
 @
@@ -26,14 +39,49 @@ class MyEnum a where
     'fromMyEnum'  :: a -> 'Integer'
 @
 -}
+class MyEnum (a :: Type) where
+    cardinality :: Proxy a -> Integer
+    default cardinality :: (Generic a, GMyEnum (Rep a)) => Proxy a -> Integer
+    cardinality _ = gcardinality (Proxy :: Proxy (Rep a))
 
+    
 
-{- $version1
--}
+    toMyEnum :: Integer -> a
+    default toMyEnum :: (Generic a, GMyEnum (Rep a)) => Integer -> a
+    toMyEnum = to . toGMyEnum
+
+    fromMyEnum :: a -> Integer
+    default fromMyEnum :: (Generic a, GMyEnum (Rep a)) => a -> Integer
+    fromMyEnum = fromGMyEnum . from
+
+instance MyEnum Void
+instance MyEnum ()
+instance MyEnum Bool
+
 class GMyEnum f where
     gcardinality :: Proxy f -> Integer
     toGMyEnum :: Integer -> f a
     fromGMyEnum :: f a -> Integer
+
+instance GMyEnum V1 where
+    gcardinality _ = 0
+    toGMyEnum _ = error "GFinite: V1 has no inhabitants"
+    fromGMyEnum _ = error "GMyEnum: V1 has no inhabitants"
+
+instance GMyEnum U1 where
+    gcardinality _ = 1    
+    toGMyEnum = const U1
+    fromGMyEnum = const 0
+
+instance (GMyEnum a) => GMyEnum (M1 _x _y a) where
+    gcardinality _ = gcardinality (Proxy @a)
+    toGMyEnum = M1 . toGMyEnum
+    fromGMyEnum = fromGMyEnum . unM1
+
+instance (MyEnum a) => GMyEnum (K1 _x a) where
+    gcardinality _ = cardinality (Proxy @a)
+    toGMyEnum = K1 . toMyEnum
+    fromGMyEnum = fromMyEnum . unK1
 
 instance (GMyEnum a, GMyEnum b) => GMyEnum (a :+: b) where
     gcardinality _ = gcardinality (Proxy @a) + gcardinality (Proxy @b)
@@ -56,3 +104,7 @@ instance (GMyEnum a, GMyEnum b) => GMyEnum (a :*: b) where
 
     fromGMyEnum (q :*: r) =
         gcardinality (Proxy @b) * fromGMyEnum q + fromGMyEnum r
+
+
+{- $version1
+-}
---
+++ src/Demo.hs
@@ -0,0 +1,20 @@
+module Demo where
+
+import Data.Void ( Void )
+import GHC.Generics ( Generic )
+import Data.Proxy ( Proxy(..) )
+import Cardinality
+
+data Action = Code Bool | Eat Bool Bool | Sleep ()
+    deriving stock (Show, Generic)
+    deriving anyclass (MyEnum)
+
+demo :: IO ()
+demo = do
+    print $ cardinality (Proxy @Void)
+    print $ cardinality (Proxy @())
+    print $ cardinality (Proxy @Bool)
+    let n =  cardinality (Proxy @Action)
+    print n
+    print (map toMyEnum [ 0..n-1 ] :: [ Action ])
+
